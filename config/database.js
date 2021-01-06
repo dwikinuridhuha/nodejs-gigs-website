22cdda1e3ea3b4843e3ed06a7e07f21a72869e28
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2021-01-06 10:56:50 +0700

    done

--- config/database.js
+++ config/database.js
@@ -1,17 +1,33 @@
 const Sequelize = require('sequelize');
-const dotenv = require('dotenv');
+module.exports = new Sequelize("express_gigs", "root", "", {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
 
-dotenv.config();
-
-module.exports =  new Sequelize(process.env.DATABASE_URL, {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: false,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
 });
+
+
+// this is old
+// const Sequelize = require('sequelize');
+// const dotenv = require('dotenv');
+//
+// dotenv.config();
+//
+// module.exports =  new Sequelize(process.env.DATABASE_URL, {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   operatorsAliases: false,
+//
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+// });
--- models/Gig.js
+++ models/Gig.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../config/database');
 
-const Gig = db.define('gig', {
+const Gigs = db.define('gigs', {
   title: {
     type: Sequelize.STRING
   },
@@ -19,7 +19,31 @@ const Gig = db.define('gig', {
   }
 });
 
-Gig.sync().then(() => {
-  console.log('table created');
-});
-module.exports = Gig;
+module.exports = Gigs;
+
+//this is old
+// const Sequelize = require('sequelize');
+// const db = require('../config/database');
+//
+// const Gig = db.define('gig', {
+//   title: {
+//     type: Sequelize.STRING
+//   },
+//   technologies: {
+//     type: Sequelize.STRING
+//   },
+//   description: {
+//     type: Sequelize.STRING
+//   },
+//   budget: {
+//     type: Sequelize.STRING
+//   },
+//   contact_email: {
+//     type: Sequelize.STRING
+//   }
+// });
+//
+// Gig.sync().then(() => {
+//   console.log('table created');
+// });
+// module.exports = Gig;
